37a69304e6655d82029f71ccb1bf356ca9b551bc
Jeffrey Byrne <jebyrne@gmail.com>
2019-10-25 19:05:06 -0400

    all-conv cifar10

--- cifar10.py
+++ cifar10.py
@@ -45,9 +45,50 @@ class LeNet(nn.Module):
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])                             
 
 
-def validate(cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth'):
-    net = LeNet()
+class AllConvNet(nn.Module):
+    """https://github.com/StefOe/all-conv-pytorch/blob/master/cifar10.ipynb"""
+    def __init__(self, input_size, n_classes=10, **kwargs):
+        super(AllConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(input_size, 96, 3, padding=1)
+        self.conv2 = nn.Conv2d(96, 96, 3, padding=1)
+        self.conv3 = nn.Conv2d(96, 96, 3, padding=1, stride=2)
+        self.conv4 = nn.Conv2d(96, 192, 3, padding=1)
+        self.conv5 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv6 = nn.Conv2d(192, 192, 3, padding=1, stride=2)
+        self.conv7 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv8 = nn.Conv2d(192, 192, 1)
 
+        self.class_conv = nn.Conv2d(192, n_classes, 1)
+
+
+    def forward(self, x):
+        x_drop = F.dropout(x, .2)
+        conv1_out = F.relu(self.conv1(x_drop))
+        conv2_out = F.relu(self.conv2(conv1_out))
+        conv3_out = F.relu(self.conv3(conv2_out))
+        conv3_out_drop = F.dropout(conv3_out, .5)
+        conv4_out = F.relu(self.conv4(conv3_out_drop))
+        conv5_out = F.relu(self.conv5(conv4_out))
+        conv6_out = F.relu(self.conv6(conv5_out))
+        conv6_out_drop = F.dropout(conv6_out, .5)
+        conv7_out = F.relu(self.conv7(conv6_out_drop))
+        conv8_out = F.relu(self.conv8(conv7_out))
+
+        class_out = F.relu(self.class_conv(conv8_out))
+        pool_out = F.adaptive_avg_pool2d(class_out, 1)
+        pool_out.squeeze_(-1)
+        pool_out.squeeze_(-1)
+        return pool_out
+
+    @staticmethod
+    def transform():
+        return transforms.Compose([transforms.Resize( (32,32) ),
+                                   transforms.ToTensor(),       
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])                             
+
+
+
+def validate(net, cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth'):
     testset = torchvision.datasets.CIFAR10(root=cifardir, train=False, download=True, transform=net.transform())
     testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
@@ -68,10 +109,8 @@ def validate(cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pt
     print("Mean classification accuracy = %f" % (correct/total))
 
 
-def train(cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth', epochs=40, lr=0.001):
+def train(net, cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth', epochs=40, lr=0.001):
     """https://github.com/pytorch/examples/tree/master/mnist"""
-    net = LeNet()
-
     trainset = torchvision.datasets.CIFAR10(root=cifardir, train=True, download=True, transform=net.transform())
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
@@ -80,6 +119,7 @@ def train(cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth',
     dataiter = iter(trainloader)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[200, 250, 300], gamma=0.1)
     time0 = time()
 
     for e in range(epochs):
@@ -94,9 +134,18 @@ def train(cifardir='/proj/enigma', modelfile='/proj/enigma/jebyrne/cifar10.pth',
         else:
             print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
 
+        scheduler.step()
         print("Training Time (in minutes) =",(time()-time0)/60)
 
     torch.save(net.state_dict(), modelfile)
 
 
 
+def lenet():
+    train(net=LeNet(), modelfile='/proj/enigma/jebyrne/cifar.pth', lr=0.001, epochs=80)
+    validate(net=LeNet(),  modelfile='/proj/enigma/jebyrne/cifar.pth')
+
+
+def allconv():
+    train(net=AllConvNet(3), modelfile='/proj/enigma/jebyrne/cifar_allconv.pth', lr=0.001, epochs=350)
+    validate(net=AllConvNet(3),  modelfile='/proj/enigma/jebyrne/cifar_allconv.pth')
