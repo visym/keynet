f6b2c965582beae0179a9f48a5aa1922015bf848
Byrne <jeff@visym.com>
2020-03-27 13:51:19 -0400

    demo

---
+++ keynet/version.py
@@ -0,0 +1,22 @@
+MAJOR = 0
+MINOR = 2
+RELEASE = 4
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, RELEASE)
+
+
+def at_least_version(versionstring):
+    """Is versionstring='X.Y.Z' at least the current version?"""
+    (major, minor, release) = versionstring.split('.')
+    return at_least_major_version(major) and at_least_minor_version(minor) and at_least_release_version(release)
+
+
+def at_least_major_version(major):
+    return MAJOR >= int(major)
+
+
+def at_least_minor_version(minor):
+    return MINOR >= int(minor)
+
+
+def at_least_release_version(release):
+    return RELEASE >= int(release)
