049168d5154ee69dfde77a2c11c853609d0e29da
Jeffrey Byrne <jeff@visym.com>
2020-03-27 11:40:50 -0400

    updated demo

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
-from keynet.layers import PermutationKeynet, PermutationKeyedSensor
+import vipy
+from keynet.system import PermutationKeynet
 from keynet.mnist import LeNet_AvgPool
 
 def challenge_problem():
@@ -11,17 +12,21 @@ def mnist():
     net = LeNet_AvgPool()
     net.load_state_dict(torch.load('./models/mnist_lenet_avgpool.pth'));
     inshape = (1,28,28)
+
+    (sensor, model) = PermutationKeynet(inshape, net, do_output_encryption=False)
     
-    sensor = PermutationKeyedSensor(inshape)
-    keynet = PermutationKeynet(net, inshape, inkey=sensor.key(), do_encrypted_output=False)
     
-    img = torch.randn(1, *inshape)
-    img_cipher = sensor.encrypt(x).tensor()
+    img_plain = sensor.load('mnist_zero.jpg').tensor()
+    img_cipher = sensor.load('mnist_zero.jpg').encrypt().tensor()
+
     yh = keynet.forward(img_cipher).detach().numpy().flatten()
-    y = net.forward(img).detach().numpy().flatten()
+    y = net.forward(img_plain).detach().numpy().flatten()
     
     assert np.allclose(y, yh, atol=1E-5)
 
+    im_cipher = sensor.encrypt().image().show()
+    im_plain = sensor.decrypt().image().show()
+    
     
 if __name__ == '__main__':
     mnist()
